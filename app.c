08b43f18627967f526303910346c4260c327b463
toxapex12163 <leonardo.ort14@gmail.com>
2025-11-19 17:20:03 -0700

    Implement main function with TODOs for enhancements
    Added main function with TODOs for screen and text color management.

---
+++ app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+// TODO: Add the include directive for the custom library header file here
+
+int main(void) {
+    
+    // TODO: Call the function from the library to clear the screen
+    void clear_screen();
+    // TODO: Call the function to print a divider line
+void print_divider();
+    // TODO: Set the text color to GREEN using the library function
+    void set_text_color(GREEN);
+    printf("System Status: ONLINE\n");
+
+    // TODO: Set the text color to RED using the library function
+    void set_text_color(RED);
+    printf("Alert: Modular compilation in progress...\n");
+
+    // TODO: Reset the text color back to default
+    void reset_text_color();
+    // TODO: Print one last divider line
+void print_divider();
+    return 0;
+}
